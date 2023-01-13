052c64fcdd4c6175b2f06ed26c6824c76952f5b5
elinanomade <elinanomad@gmail.com>
2023-01-13 12:40:59 +0100

    Created Form

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -14,4 +14,19 @@ function NewMeetupForm() {
           <input type="url" required id="image" />
         </div>
         <div className={classes.control}>
-          <label htmlFor="a
+          <label htmlFor="address"> Address</label>
+          <input type="text" required id="address" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="description">Description</label>
+          <textarea id="description" required rows="5"></textarea>
+        </div>
+        <div className={classes.actions}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
+    </Card>
+  );
+}
+
+export default NewMeetupForm;
